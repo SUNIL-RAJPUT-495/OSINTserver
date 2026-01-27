321cbd6999d9f603d40d51b48d2b983bdea7b8c0
sunil-495 <sunilsinghshekhawat495@gmail.com>
2026-01-27 13:17:57 +0530

    all ok

--- index.js
+++ index.js
@@ -1,35 +1,39 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import {connectDB} from '../backend/config/db.js';
+import { connectDB } from './config/db.js'; // Check path carefully
 import userRouter from './routes/user.route.js';
 import morgan from 'morgan'; 
 import cors from 'cors';  
 import roomRouter from './routes/room.router.js';
 import challengRouter from './routes/challeng.router.js';
+
+dotenv.config();
 const app = express();
+
 app.use(express.json());
 app.use(morgan('dev'));
 
 app.use(cors({
-    origin: "http://localhost:5173", 
+    origin: process.env.FRONTEND_URL || "http://localhost:5173", 
     credentials: true,             
     methods: ["GET", "POST", "PUT", "DELETE"], 
 }));
-dotenv.config();
-
-
-const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World! dfd');
+  res.send('Cyber API is running...');
 });
 
-app.use("/api/user",userRouter)
-app.use("/api/room",roomRouter);
-app.use("/api/challenge",challengRouter);
-
+app.use("/api/user", userRouter);
+app.use("/api/room", roomRouter);
+app.use("/api/challenge", challengRouter);
 
 connectDB();
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+
+if (process.env.NODE_ENV !== 'production') {
+    const PORT = process.env.PORT || 8080;
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}
+
+export default app;
