b8c9489613d6eb1363ce5893cb7bca98f04454ac
sunil-495 <sunilsinghshekhawat495@gmail.com>
2026-01-27 13:29:01 +0530

    frountend url

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ const app = express();
 
 app.use(express.json());
 app.use(morgan('dev'));
-
 app.use(cors({
     origin: process.env.FRONTEND_URL || "http://localhost:5173", 
     credentials: true,             
