61ae93910d025c3e8a88ec989b3f05a45cfd465d
sunil-495 <sunilsinghshekhawat495@gmail.com>
2026-01-30 14:33:42 +0530

    all done

--- routes/challeng.router.js
+++ routes/challeng.router.js
@@ -12,7 +12,7 @@ const challengRouter = Router();
 
 challengRouter.post("/CreateChallenge", authToken, creatChallange);
 challengRouter.get("/getchallenges/:id", getChallengesByRoom);
-challengRouter.post("/submitchallenge",  submitChallenge);
+challengRouter.post("/submitchallenge", authToken, submitChallenge);
 
 challengRouter.put("/updatechallenge/:challengeId", authToken, updateChallenge);
 challengRouter.delete("/deletechallenge/:challengeId", authToken, deleteChallenge);
